67677822d162d13ce5284b17c5bb9dee66d8195a
KavinPrasath01 <kavinrathinasamycs@gmail.com>
2024-09-21 08:09:37 +0530

    new commit

---
+++ src/org/test/MyPlace.java
@@ -0,0 +1,18 @@
+package org.test;
+
+public class MyPlace {
+	
+	private void myVillage() {
+		System.out.println("Sathyamangalam");		
+	}
+	
+	private void mycollege() {
+		System.out.println("Erode");
+	}
+
+	public static void main(String[] args) {
+		MyPlace p = new MyPlace();
+		p.myVillage();
+		p.mycollege();
+	}
+}
